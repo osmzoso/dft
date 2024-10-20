3e514df5fcd7ef80e6e91a53f5aaa4de23faf96a
osmzoso <62518847+osmzoso@users.noreply.github.com>
2024-10-20 13:53:52 +0200

    Add files via upload

--- dft.py
+++ dft.py
@@ -3,6 +3,7 @@
 Discrete Fourier Transform DFT
 """
 import math
+import cmath
 
 
 def dft(x):
@@ -45,6 +46,43 @@ def idft(X):
         imag = imag / N
         x[n] = complex(real, imag)
     return x
+
+
+#
+# dft_exp() and idft_exp() uses the complex exp() function in modul cmath
+#
+
+
+def dft_exp(x):
+    """
+    Compute the Discrete Fourier Transform (DFT)
+
+    Parameter x : The time-domain signal as a list of complex numbers.
+    Returns   X : List of DFT coefficients (frequency-domain representation).
+    """
+    N = len(x)
+    X = [complex()] * N
+    for k in range(N):  # Loop over each frequency bin
+        for n in range(N):  # Loop over each time sample
+            X[k] += x[n] * cmath.exp(-2j * cmath.pi * k * n / N)
+    return X
+
+
+def idft_exp(X):
+    """
+    Compute the Inverse Discrete Fourier Transform (IDFT)
+
+    Parameter X : List of DFT coefficients (frequency-domain representation).
+    Returns   x : The reconstructed sequence in the time-domain.
+    """
+    N = len(X)
+    x = [complex()] * N
+    for n in range(N):
+        for k in range(N):
+            x[n] += X[k] * cmath.exp(2j * cmath.pi * k * n / N)
+        x[n] = x[n] / N  # Normalization factor
+    return x
+
 
 if __name__ == '__main__':
     pass
